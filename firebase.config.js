549d4537922b768ffe76b6237a69254ef38ee4bb
Arvind Yadav <=>
2023-07-07 11:19:03 +0530

    Initial commit on Stack Player

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXmkMzWHCloFLxcKkTfHlpTvhOsIlBguY",
+  authDomain: "mediax-c3a0b.firebaseapp.com",
+  projectId: "mediax-c3a0b",
+  storageBucket: "mediax-c3a0b.appspot.com",
+  messagingSenderId: "958954617576",
+  appId: "1:958954617576:web:80752b759dbeaedb78e75d"
+};
+
+// Initialize Firebase
+export default  app = initializeApp(firebaseConfig);
