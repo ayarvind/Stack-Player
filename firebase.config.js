de212bf53ba1482ff29c8a65d2b07252a4dfbf4e
Arvind Yadav <ayarvind9812@gmail.com>
2023-07-07 12:28:16 +0530

    Update firebase.config.js
    Removed some private credentials from firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAXmkMzWHCloFLxcKkTfHlpTvhOsIlBguY",
-  authDomain: "mediax-c3a0b.firebaseapp.com",
-  projectId: "mediax-c3a0b",
-  storageBucket: "mediax-c3a0b.appspot.com",
-  messagingSenderId: "958954617576",
-  appId: "1:958954617576:web:80752b759dbeaedb78e75d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
